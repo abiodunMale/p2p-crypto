66611dbc0e68a47170224684908edd89f45a6a9c
unknown <abiodun.samuel@feghas.com>
2021-04-07 17:45:26 +0100

    create account

--- backend/controller/auth.js
+++ backend/controller/auth.js
@@ -1,5 +1,20 @@
 const userModel = require('../models/user');
 
-exports.Register = (req, res) => {
-    res.json({msg: 'resgister end point', data: req.body});
+exports.Register = async (req, res) => {
+
+    const user = req.body;
+    try {
+
+        const checkIfExist =  await userModel.findOne({emailaddress: user.emailaddress});
+        if(checkIfExist){
+            return res.status(409).json({message: 'user already exist'})
+        }
+
+        const newUser = new userModel(user);
+        await newUser.save();
+        return res.status(200).json({message: 'account created successfully', user: newUser});
+        
+    } catch (error) {
+        return res.status(500).json({message: 'an error occured '+error})
+    }
 };
